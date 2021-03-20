4696866cc802e102af51013b34ba59540d21ae80
Elder <elucas-2015337@kinal.edu.gt>
2021-03-19 20:16:15 -0600

    Vista para bonificaciones y descuentos

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,10 @@ import exphbs from 'express-handlebars';
 const app = express();
 
 app.use('/static',express.static('public'));
+// app.use('/discount', require('./views'));
 
 
+app.set('port', process.env.PORT || 5000 );
 const hbs = exphbs.create({
     extname: '.hbs',
     defaultLayout: 'layout',
@@ -26,5 +28,13 @@ app.get('/',function(req,res){
     res.render('home');
 });
 
+app.get('/bonificaciones',function(req,res){
+  res.render('listaBonificaciones');
+});
 
-app.listen(5000, () => console.log('server running'));
+app.get('/descuentos',function(req,res){
+  res.render('listaDescuentos');
+});
+
+
+app.listen(app.get('port'), () => console.log('server running on port ' + app.get('port')));
