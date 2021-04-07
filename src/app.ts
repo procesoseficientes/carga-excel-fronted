52421d726e59b029b6a52eda2eea8e054aafe326
JJE <elguetaj@ufm.edu>
2021-04-07 08:57:57 -0600

    adds uploading files

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path'
-
+import fileUpload, { UploadedFile } from 'express-fileupload';
 
 import exphbs from 'express-handlebars';
 
@@ -8,6 +8,13 @@ const app = express();
 
 app.use('/static',express.static('public'));
 app.use('/src', express.static('src'));
+
+
+
+app.use(fileUpload({
+  useTempFiles : true,
+  tempFileDir : '/tmp/'
+}));
 
 
 app.set('port', process.env.PORT || 5000 );
@@ -18,6 +25,7 @@ const hbs = exphbs.create({
       json: (context: string) => JSON.stringify(context)
     }
   }) 
+
 
 app.engine('.hbs', hbs.engine)
 app.set('view engine', '.hbs')
@@ -35,6 +43,31 @@ app.get('/login',function(req,res){
 app.get('/bonificaciones',function(req,res){
   res.render('listaBonificaciones');
 });
+
+
+app.post('/upload', function(req, res) {
+  let uploadedFile:UploadedFile;
+  let uploadPath:string;
+
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  uploadedFile = req.files.uploadedFile as UploadedFile;
+  uploadPath = __dirname + '/files/' + uploadedFile.name;
+
+  // Use the mv() method to place the file somewhere on your server
+  uploadedFile.mv(uploadPath, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
+
+
+
 
 app.get('/descuentos',function(req,res){
   res.render('listaDescuentos');
