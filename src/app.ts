bea76d29fe48ccb486874acdd5e4296cbe961d07
Elder <elucas-2015337@kinal.edu.gt>
2021-04-09 13:03:44 -0600

    Message templates and loading button

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,55 @@
 import express from 'express';
 import path from 'path'
-
-
+import flash from 'connect-flash';
+import session from 'express-session'
+//const flash = require('connect-flash')
 import exphbs from 'express-handlebars';
 
 const app = express();
 
 app.use('/static',express.static('public'));
 app.use('/src', express.static('src'));
+app.use('/views', express.static('views'))
 
 
 app.set('port', process.env.PORT || 5000 );
-const hbs = exphbs.create({
-    extname: '.hbs',
-    defaultLayout: 'layout',
-    helpers: {
-      json: (context: string) => JSON.stringify(context)
-    }
-  }) 
+// const hbs = exphbs.create({
+//     extname: '.hbs',
+//     defaultLayout: 'layout',
+//     partialsDir: path.join(app.get('views'), 'partials'),
+//     helpers: {
+//       json: (context: string) => JSON.stringify(context)
+//     }
+//   }) 
 
-app.engine('.hbs', hbs.engine)
+  app.engine('.hbs', exphbs({
+      defaultLayout: 'layout',
+      //layoutsDir: path.join(app.get('views'), 'layouts'),
+      partialsDir  : [
+        //  path to your partials
+        path.join(__dirname, 'views/partials'),
+    ],
+      extname: '.hbs',
+      helpers: {
+               json: (context: string) => JSON.stringify(context)
+             }
+  }));
+
+  app.use(session({ cookie: { maxAge: 60000 }, 
+    secret: 'woot',
+    resave: false, 
+    saveUninitialized: false}));
+
+  app.use(flash());
+
+   app.use((req, res, next) => {
+     app.locals.message = req.flash('message');
+     app.locals.success = req.flash('success');
+     //app.locals.user = req.user;
+     next();
+   });
+
+//app.engine('.hbs', hbs.engine)
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
